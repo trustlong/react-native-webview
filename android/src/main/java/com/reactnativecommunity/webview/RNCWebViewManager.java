6bcd26f12c319933edfb1142058a435992bbfc7a
Long Cheng <trustlong@gmail.com>
2019-07-18 12:19:12 -0400

    fix

--- android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
+++ android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
@@ -72,6 +72,7 @@ import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
+import java.util.ArrayList;
 
 import javax.annotation.Nullable;
 
@@ -538,6 +539,42 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
   protected void setupWebChromeClient(ReactContext reactContext, WebView webView) {
     if (mAllowsFullscreenVideo) {
       mWebChromeClient = new RNCWebChromeClient(reactContext, webView) {
+
+      // Fix WebRTC permission request error.
+      @Override
+      public void onPermissionRequest(final PermissionRequest request) {
+          String[] requestedResources = request.getResources();
+          ArrayList<String> permissions = new ArrayList<>();
+          ArrayList<String> grantedPermissions = new ArrayList<String>();
+          for (int i = 0; i < requestedResources.length; i++) {
+            if (requestedResources[i].equals(PermissionRequest.RESOURCE_AUDIO_CAPTURE)) {
+              permissions.add(Manifest.permission.RECORD_AUDIO);
+            } else if (requestedResources[i].equals(PermissionRequest.RESOURCE_VIDEO_CAPTURE)) {
+              permissions.add(Manifest.permission.CAMERA);
+            }
+            // TODO: RESOURCE_MIDI_SYSEX, RESOURCE_PROTECTED_MEDIA_ID.
+          }
+
+          for (int i = 0; i < permissions.size(); i++) {
+            if (ContextCompat.checkSelfPermission(reactContext, permissions.get(i)) != PackageManager.PERMISSION_GRANTED) {
+              continue;
+            }
+            if (permissions.get(i).equals(Manifest.permission.RECORD_AUDIO)) {
+              grantedPermissions.add(PermissionRequest.RESOURCE_AUDIO_CAPTURE);
+            } else if (permissions.get(i).equals(Manifest.permission.CAMERA)) {
+              grantedPermissions.add(PermissionRequest.RESOURCE_VIDEO_CAPTURE);
+            }
+          }
+
+          if (grantedPermissions.isEmpty()) {
+            request.deny();
+          } else {
+            String[] grantedPermissionsArray = new String[grantedPermissions.size()];
+            grantedPermissionsArray = grantedPermissions.toArray(grantedPermissionsArray);
+            request.grant(grantedPermissionsArray);
+          }
+        }
+
         @Override
         public void onShowCustomView(View view, CustomViewCallback callback) {
           if (mVideoView != null) {
@@ -736,41 +773,6 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
         new TopLoadingProgressEvent(
           webView.getId(),
           event));
-    }
-
-    // Fix WebRTC permission request error.
-    @Override
-    public void onPermissionRequest(final PermissionRequest request) {
-        String[] requestedResources = request.getResources();
-        ArrayList<String> permissions = new ArrayList<>();
-        ArrayList<String> grantedPermissions = new ArrayList<String>();
-         for (int i = 0; i < requestedResources.length; i++) {
-          if (requestedResources[i].equals(PermissionRequest.RESOURCE_AUDIO_CAPTURE)) {
-            permissions.add(Manifest.permission.RECORD_AUDIO);
-          } else if (requestedResources[i].equals(PermissionRequest.RESOURCE_VIDEO_CAPTURE)) {
-            permissions.add(Manifest.permission.CAMERA);
-          }
-          // TODO: RESOURCE_MIDI_SYSEX, RESOURCE_PROTECTED_MEDIA_ID.
-        }
-
-        for (int i = 0; i < permissions.size(); i++) {
-          if (ContextCompat.checkSelfPermission(reactContext, permissions.get(i)) != PackageManager.PERMISSION_GRANTED) {
-            continue;
-          }
-          if (permissions.get(i).equals(Manifest.permission.RECORD_AUDIO)) {
-            grantedPermissions.add(PermissionRequest.RESOURCE_AUDIO_CAPTURE);
-          } else if (permissions.get(i).equals(Manifest.permission.CAMERA)) {
-            grantedPermissions.add(PermissionRequest.RESOURCE_VIDEO_CAPTURE);
-          }
-        }
-
-        if (grantedPermissions.isEmpty()) {
-          request.deny();
-        } else {
-          String[] grantedPermissionsArray = new String[grantedPermissions.size()];
-          grantedPermissionsArray = grantedPermissions.toArray(grantedPermissionsArray);
-          request.grant(grantedPermissionsArray);
-        }
     }
 
     @Override
